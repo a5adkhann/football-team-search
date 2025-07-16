a06ca30001bf186ce18da75de9fb883cefae81ba
Asad Khan <asadkaptech@gmail.com>
2025-07-16 16:57:22 +0500

    Loader Implemented

--- script.js
+++ script.js
@@ -7,29 +7,42 @@ const teamGround = document.getElementsByClassName("team-ground")[0];
 const teamCountry = document.getElementsByClassName("team-country")[0];
 const teamWeb = document.getElementsByClassName("team-web")[0];
 const teamDescription = document.getElementsByClassName("team-description")[0];
+const loader = document.querySelector(".spinner-border");
+const teamContainer = document.getElementById("team-container");
 
-const getTeamData = async(teamName) => {
+const getTeamData = async (teamName) => {
     const response = await fetch(`https://www.thesportsdb.com/api/v1/json/123/searchteams.php?t=${teamName}`);
-    console.log(response)
     return await response.json();
 }
 
-searchBtn.addEventListener("click", async() => {
-
-    if(searchInput){
-        document.getElementById("team-container").classList.remove("hide");
+searchBtn.addEventListener("click", async () => {
+    if (!searchInput.value.trim()) {
+        alert("Please enter a team name!");
+        return;
     }
-    
-    const result = await getTeamData(searchInput.value);
 
-    teamImg.setAttribute("src", result.teams[0].strBadge);
-    teamTitle.innerHTML = result.teams[0].strTeam;
-    teamBirthYear.innerHTML = result.teams[0].intFormedYear;
-    teamGround.innerHTML = result.teams[0].strStadium;
-    teamCountry.innerHTML = result.teams[0].strCountry;
-    teamWeb.setAttribute("href", `https//${result.teams[0].strWebsite}`);
-    teamDescription.innerHTML = result.teams[0].strDescriptionEN.substring(0, 200);
+    loader.classList.remove("hide"); 
+    teamContainer.classList.add("hide"); 
+
+    const result = await getTeamData(searchInput.value.trim());
+    loader.classList.add("hide"); 
+
+    const team = result.teams[0];
+
+    teamImg.setAttribute("src", team.strBadge);
+    teamTitle.innerHTML = team.strTeam;
+    teamBirthYear.innerHTML = team.intFormedYear;
+    teamGround.innerHTML = team.strStadium;
+    teamCountry.innerHTML = team.strCountry;
+    teamWeb.setAttribute("href", `https://${team.strWebsite}`);
+    teamWeb.innerHTML = team.strWebsite;
+    teamDescription.innerHTML = team.strDescriptionEN
+        ? team.strDescriptionEN.substring(0, 200) + "..."
+        : "No description available.";
+
+    teamContainer.classList.remove("hide");
+
+    loader.classList.add("hide");
 
     searchInput.value = "";
-
-})
+});
